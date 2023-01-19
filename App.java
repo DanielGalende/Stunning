acd8457aa0adae941d0fdbe0b9ae42c41b99f1be
Alumno <danigalcol@gmail.com>
2023-01-19 11:01:56 +0100

    commit fanial

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
-public class App {
+ public class App {
   
   public static void main(String[] args) {
+    System.out.println("DAniel Galende");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("19-01-2023");
   }
   
 }
